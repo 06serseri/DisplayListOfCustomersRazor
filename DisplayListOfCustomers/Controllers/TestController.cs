03349b1ede1e4617bd05edce54b2eb24e48a5976
06serseri <tunaalkan91@gmail.com>
2021-04-11 13:52:25 -0400

    Add project files.

---
+++ DisplayListOfCustomers/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using DisplayListOfCustomers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DisplayListOfCustomers.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            List<UserModel> users = new List<UserModel>();
+            users.Add(new UserModel("Polat Alemdar", "polat@gmail.com", "(123) 456-7890)"));
+            users.Add(new UserModel("Ivan Drago", "ivan@gmail.com", "(123) 456-7890)"));
+            users.Add(new UserModel("Memati Bas", "memati@gmail.com", "(123) 456-7890)"));
+            users.Add(new UserModel("Gunnary Sergaent Hartman", "hartman@gmail.com", "(123) 456-7890)"));
+             
+
+            return View("Test", users);
+        }
+    }
+}
---
+++ DisplayListOfCustomers/Models/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DisplayListOfCustomers.Models
+{
+    public class UserModel
+    {
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public UserModel(string name, string emailAddress, string phoneNumber)
+        {
+            Name = name;
+            EmailAddress = emailAddress;
+            PhoneNumber = phoneNumber;
+        }
+
+
+
+    }
+}
